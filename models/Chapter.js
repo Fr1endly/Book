89c3b25265d1b3711320364ceb992eae4c232519
Oskar <GizatulinsO@gmail.com>
2020-03-30 08:01:51 +0300

    laptop 30 march

--- models/Chapter.js
+++ models/Chapter.js
@@ -1,27 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const chapterSchema = new mongoose.Schema({
-        title: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        index: {
-            type: Number,
-            required: true
-        },
-        sections: [
-            {
-                index: Number,
-                title: {
-                    type: String,
-                    required: true,
-                    unique: true
-                },
-                text: String // Slate state object 
-            }
-        ]
-})
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  index: {
+    type: Number,
+    required: true
+  },
+  sections: {
+    type: String
+  }
+});
 
-const Chapter = mongoose.model('chapter', chapterSchema)
+const Chapter = mongoose.model("chapter", chapterSchema);
 export default Chapter;
