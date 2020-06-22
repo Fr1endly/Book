b1f1fed3d361c37e18052b5cc925464a47b9969f
Oskar <GizatulinsO@gmail.com>
2020-06-22 21:17:42 +0300

    Laptop 22june

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,6 +4,8 @@ import auth from "../../middleware/auth";
 import User from "../../models/User";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
+import bigInt from "big-integer";
 
 const router = Router();
 
@@ -27,7 +29,7 @@ router.post(
   "/",
   [
     check("email", "Please provide valid email.").isEmail(),
-    check("password", "Password is required").exists()
+    check("password", "Password is required").exists(),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -60,8 +62,8 @@ router.post(
 
       const payload = {
         user: {
-          id: user.id
-        }
+          id: user.id,
+        },
       };
       jwt.sign(
         payload,
@@ -78,5 +80,91 @@ router.post(
     }
   }
 );
+
+// Route api/auth/reset
+// Desc check for user email and send password reset link
+// Acc Public
+router.post(
+  "/reset",
+  [check("email", "Please provide valid email.").isEmail()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email } = req.body;
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "rollnroleofficial@gmail.com",
+        pass: "Oskar007",
+      },
+    });
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        res.status(400).json({ errors: [{ msg: "Invalid credentials." }] });
+      }
+
+      const ident = parseInt(user.name, 36);
+      console.log(ident, ident.toString(36));
+      const time = new Date().getTime().toString(36);
+
+      const string = `${user._id}${user.lastLoginDate}${user.password}`;
+      const salt = await bcrypt.genSalt(12);
+      const hash = await bcrypt.hash(string, salt);
+
+      var mailOptions = {
+        from: "rollnroleofficial@gmail.com",
+        to: "rollnroleofficial@gmail.com",
+        subject: "Password reset",
+        text: `Password reset link: http://localhost:3000/api/auth/reset/${ident}/${time}-${hash}`,
+      };
+
+      const info = await transporter.sendMail(mailOptions);
+
+      res.json({ msg: `Message sent: ${info.messageId}` });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+// route api/auth/reset/:ident/:time-:hash
+// desc Validate link and on success send new randomized password
+// acc public
+router.get("/reset/:ident/:time-:hash", async (req, res) => {
+  const { ident, time, hash } = req.params;
+
+  const serverTime = new Date().getTime();
+  const dayInSeconds = 86400000;
+
+  const name = ident.toString(36);
+
+  console.log(ident.toString(36));
+
+  if (serverTime - parseInt(time, 36) > dayInSeconds) {
+    res.status(400).json({ errors: [{ msg: "Outdated link." }] });
+  }
+
+  try {
+    const user = await User.findOne({ name });
+    if (!user) {
+      res.status(400).json({ errors: [{ msg: "Invalid credentials" }] });
+    }
+
+    const str = `${user._id}${user.lastLoginDate}${user.password}`;
+
+    const isMatch = await bcrypt.compare(str, hash);
+    console.log(isMatch);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 export default router;
